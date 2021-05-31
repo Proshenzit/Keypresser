24e8d2954b16e6bd6c4bb06ad44e6602754f5596
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-31 02:33:48 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,28 @@
+
+var count=0;
+
+document.querySelector("textarea"). addEventListener("keypress",function(event){
+
+
+    count++;
+
+    var text=event.key;
+
+
+    document.querySelector("p").innerHTML="You Have Pressd " +count;
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
